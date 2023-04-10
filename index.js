2b8aba19e09262a42cedf2946b8affa13f5daa9a
srjizhere <bhiapatil@gmail.com>
2023-04-10 18:34:11 +0530

    first_version

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+console.log(`Full Stack Developer
+with core competence in NodeJs 
+
+github: https://github.com/srjizhere
+linkedin: https://www.linkedin.com/in/srjizhere/
+resume: https://drive.google.com/file/d/10UAxghODAgPYhwTjCZWcReh4yZF3CMAm/view?usp=share_link
+email: bhiapatil@gmail.com
+`);
